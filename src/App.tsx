d15e830d592b86048d304a8deafa6071ff9ed1b3
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-02-12 22:48:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
           <Sidebar />
         </div>
         <Routes>
-          <Route path="/dashboard" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
         </Routes>
       </div>
